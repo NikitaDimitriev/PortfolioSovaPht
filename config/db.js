1c8f073389ffdf8433907d161dbaf521a69aefd1
NikitaDimitriev <nikitadimitriev000@gmail.com>
2017-11-11 13:43:55 +0200

    Com

--- config/db.js
+++ config/db.js
@@ -1,7 +1,3 @@
 module.exports = {
-	host : process.env['MONGODB_ADDON_HOST'],
-	user : process.env['MONGODB_ADDON_USER'],
-	password: process.env['MONGODB_ADDON_USER'],
-	port: process.env['MONGODB_ADDON_PORT'],
-	uri : process.env['MONGODB_ADDON_URI']
+	url : 'mongodb://localhost:27017/PorfolioDB'
 }
